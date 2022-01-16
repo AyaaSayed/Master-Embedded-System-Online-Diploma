d0793449940926e604cdb21e83f2218185fa4d60
AyaaSayed <95402835+AyaaSayed@users.noreply.github.com>
2022-01-16 02:34:09 +0200

    Delete Unit 3 ( Embedded C )/Assignment directory

--- Unit 3 ( Embedded C )/Assignment/HW 2/Lab 1/Startup.s
+++
@@ -1,5 +0,0 @@
-.global reset
-reset:
-         ldr  sp, = stack_top
-         bl   main
-stop:    b    stop
--- Unit 3 ( Embedded C )/Assignment/HW 2/Lab 1/UART.h
+++
@@ -1,6 +0,0 @@
-#ifndef _UART_H_
-#define _UART_H_
-
-void UART_SEND_DATA( unsigned char* p_tx_string );
-
-#endif
