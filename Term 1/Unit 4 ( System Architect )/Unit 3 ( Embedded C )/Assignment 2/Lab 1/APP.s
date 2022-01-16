f2bcef68ca72064259b51c76d99bbb52a921bca8
AyaaSayed <95402835+AyaaSayed@users.noreply.github.com>
2022-01-17 00:11:03 +0200

    updates

---
+++ Term 1/Unit 4 ( System Architect )/Unit 3 ( Embedded C )/Assignment 2/Lab 1/APP.s
@@ -0,0 +1,82 @@
+
+APP.o:     file format elf32-littlearm
+
+
+Disassembly of section .text:
+
+00000000 <main>:
+   0:	e92d4800 	push	{fp, lr}
+   4:	e28db004 	add	fp, sp, #4
+   8:	e59f000c 	ldr	r0, [pc, #12]	; 1c <main+0x1c>
+   c:	ebfffffe 	bl	0 <UART_SEND_DATA>
+  10:	e3a03000 	mov	r3, #0
+  14:	e1a00003 	mov	r0, r3
+  18:	e8bd8800 	pop	{fp, pc}
+  1c:	00000000 	andeq	r0, r0, r0
+
+Disassembly of section .data:
+
+00000000 <string_buffer>:
+   0:	7261654c 	rsbvc	r6, r1, #76, 10	; 0x13000000
+   4:	6e695f6e 	cdpvs	15, 6, cr5, cr9, cr14, {3}
+   8:	7065645f 	rsbvc	r6, r5, pc, asr r4
+   c:	3c206874 	stccc	8, cr6, [r0], #-464	; 0xfffffe30
+  10:	61794120 	cmnvs	r9, r0, lsr #2
+  14:	79615320 	stmdbvc	r1!, {r5, r8, r9, ip, lr}^
+  18:	3e206465 	cdpcc	4, 2, cr6, cr0, cr5, {3}
+	...
+
+Disassembly of section .comment:
+
+00000000 <.comment>:
+   0:	43434700 	movtmi	r4, #14080	; 0x3700
+   4:	4728203a 			; <UNDEFINED> instruction: 0x4728203a
+   8:	5420554e 	strtpl	r5, [r0], #-1358	; 0x54e
+   c:	736c6f6f 	cmnvc	ip, #444	; 0x1bc
+  10:	726f6620 	rsbvc	r6, pc, #32, 12	; 0x2000000
+  14:	6d724120 	ldfvse	f4, [r2, #-128]!	; 0xffffff80
+  18:	626d4520 	rsbvs	r4, sp, #32, 10	; 0x8000000
+  1c:	65646465 	strbvs	r6, [r4, #-1125]!	; 0x465
+  20:	72502064 	subsvc	r2, r0, #100	; 0x64
+  24:	7365636f 	cmnvc	r5, #-1140850687	; 0xbc000001
+  28:	73726f73 	cmnvc	r2, #460	; 0x1cc
+  2c:	322d3720 	eorcc	r3, sp, #32, 14	; 0x800000
+  30:	2d373130 	ldfcss	f3, [r7, #-192]!	; 0xffffff40
+  34:	6d2d3471 	cfstrsvs	mvf3, [sp, #-452]!	; 0xfffffe3c
+  38:	726f6a61 	rsbvc	r6, pc, #397312	; 0x61000
+  3c:	2e372029 	cdpcs	0, 3, cr2, cr7, cr9, {1}
+  40:	20312e32 	eorscs	r2, r1, r2, lsr lr
+  44:	37313032 			; <UNDEFINED> instruction: 0x37313032
+  48:	34303930 	ldrtcc	r3, [r0], #-2352	; 0x930
+  4c:	65722820 	ldrbvs	r2, [r2, #-2080]!	; 0x820
+  50:	7361656c 	cmnvc	r1, #108, 10	; 0x1b000000
+  54:	5b202965 	blpl	80a5f0 <main+0x80a5f0>
+  58:	2f4d5241 	svccs	0x004d5241
+  5c:	65626d65 	strbvs	r6, [r2, #-3429]!	; 0xd65
+  60:	64656464 	strbtvs	r6, [r5], #-1124	; 0x464
+  64:	622d372d 	eorvs	r3, sp, #11796480	; 0xb40000
+  68:	636e6172 	cmnvs	lr, #-2147483620	; 0x8000001c
+  6c:	65722068 	ldrbvs	r2, [r2, #-104]!	; 0x68
+  70:	69736976 	ldmdbvs	r3!, {r1, r2, r4, r5, r6, r8, fp, sp, lr}^
+  74:	32206e6f 	eorcc	r6, r0, #1776	; 0x6f0
+  78:	30323535 	eorscc	r3, r2, r5, lsr r5
+  7c:	Address 0x0000007c is out of bounds.
+
+
+Disassembly of section .ARM.attributes:
+
+00000000 <.ARM.attributes>:
+   0:	00003141 	andeq	r3, r0, r1, asr #2
+   4:	61656100 	cmnvs	r5, r0, lsl #2
+   8:	01006962 	tsteq	r0, r2, ror #18
+   c:	00000027 	andeq	r0, r0, r7, lsr #32
+  10:	4d524105 	ldfmie	f4, [r2, #-20]	; 0xffffffec
+  14:	45363239 	ldrmi	r3, [r6, #-569]!	; 0x239
+  18:	00532d4a 	subseq	r2, r3, sl, asr #26
+  1c:	01080506 	tsteq	r8, r6, lsl #10
+  20:	04120109 	ldreq	r0, [r2], #-265	; 0x109
+  24:	01150114 	tsteq	r5, r4, lsl r1
+  28:	01180317 	tsteq	r8, r7, lsl r3
+  2c:	011a0119 	tsteq	sl, r9, lsl r1
+  30:	Address 0x00000030 is out of bounds.
+
---
+++ Term 1/Unit 4 ( System Architect )/Unit 3 ( Embedded C )/Assignment 2/Lab 1/UART.h
@@ -0,0 +1,6 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+void UART_SEND_DATA( unsigned char* p_tx_string );
+
+#endif
