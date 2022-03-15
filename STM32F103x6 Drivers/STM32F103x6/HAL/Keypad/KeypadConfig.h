3157406dcf14e335c5031d1ac867027e0ce87a0b
Aya Sayed <95402835+AyaaSayed@users.noreply.github.com>
2022-03-15 02:20:16 +0200

    Update KeypadConfig.h

--- STM32F103x6 Drivers/STM32F103x6/HAL/Keypad/KeypadConfig.h
+++ STM32F103x6 Drivers/STM32F103x6/HAL/Keypad/KeypadConfig.h
@@ -15,10 +15,10 @@
 
 #define KEYPAD_PORT  	GPIOA
 
-#define ROW0			  GPIO_PIN_8
-#define ROW1			  GPIO_PIN_9
-#define ROW2			  GPIO_PIN_10
-#define ROW3		  	GPIO_PIN_11
+#define ROW0			GPIO_PIN_8
+#define ROW1			GPIO_PIN_9
+#define ROW2			GPIO_PIN_10
+#define ROW3			GPIO_PIN_11
 #define COLUM0			GPIO_PIN_12
 #define COLUM1			GPIO_PIN_13
 #define COLUM2			GPIO_PIN_14
