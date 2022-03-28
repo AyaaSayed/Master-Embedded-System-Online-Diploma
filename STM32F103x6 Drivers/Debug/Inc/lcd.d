0b42c97627e1c619d7e7239fff16148eee60762f
Aya Sayed <95402835+AyaaSayed@users.noreply.github.com>
2022-03-28 09:23:39 +0200

    Delete STM32F103x6 Drivers directory

--- STM32F103x6 Drivers/Debug/Inc/lcd.d
+++
@@ -1,9 +0,0 @@
-Inc/lcd.o: ../Inc/lcd.c ../Inc/lcd.h \
- D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/GPIO.h \
- D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/STM32F103x6.h
-
-../Inc/lcd.h:
-
-D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/GPIO.h:
-
-D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/STM32F103x6.h:
--- STM32F103x6 Drivers/Debug/STM32F103x6/Src/GPIO.d
+++
@@ -1,7 +0,0 @@
-STM32F103x6/Src/GPIO.o: ../STM32F103x6/Src/GPIO.c \
- D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/GPIO.h \
- D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/STM32F103x6.h
-
-D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/GPIO.h:
-
-D:/my\ work/master\ embedded\ systems\ diploma/Unit\ 7\ (MCU\ Essential\ Peripherals\ )/Lesson\ 3/Assignment/Drivers/STM32F103x6/Inc/STM32F103x6.h:
