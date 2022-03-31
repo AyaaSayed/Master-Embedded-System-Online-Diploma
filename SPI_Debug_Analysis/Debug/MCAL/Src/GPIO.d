74e367a5c1d1a40ad3afb9c8f07ba6855e49831f
Aya Sayed <95402835+AyaaSayed@users.noreply.github.com>
2022-03-31 15:08:00 +0200

    Delete SPI_Debug_Analysis directory

--- SPI_Debug_Analysis/Debug/MCAL/Src/GPIO.d
+++
@@ -1,7 +0,0 @@
-MCAL/Src/GPIO.o: ../MCAL/Src/GPIO.c \
- D:/my\ work/master\ embedded\ systems\ diploma/Drivers/STM32F103C6\ Drivers/MCAL/Inc/GPIO.h \
- D:/my\ work/master\ embedded\ systems\ diploma/Drivers/STM32F103C6\ Drivers/Inc/STM32F103x6.h
-
-D:/my\ work/master\ embedded\ systems\ diploma/Drivers/STM32F103C6\ Drivers/MCAL/Inc/GPIO.h:
-
-D:/my\ work/master\ embedded\ systems\ diploma/Drivers/STM32F103C6\ Drivers/Inc/STM32F103x6.h:
--- SPI_Debug_Analysis/Readme.md
+++
@@ -1,3 +0,0 @@
-# ATmega32 Drivers
-## SPI Protocals
-### Proteus Simulation for SPI with MAX7221: [Click Here](https://drive.google.com/file/d/1pmPwq_FivnOalanbPDMJmR_b38SKhiKy/view)
